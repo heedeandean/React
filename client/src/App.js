5b0aa60a53d5431a8d39b6ab9983a24c6dc922e4
heedeandean <7942ahj@hanmail.net>
2021-02-26 08:56:22 +0900

    2021-02-26

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import SetState from './SetState';
+import ReturnMap from './ReturnMap';
 
 function App() {
   return (
     <div>
       <h1>Start React</h1>
       <p>css</p>
-      <SetState />
+      <ReturnMap/>
     </div>
   );
 }
