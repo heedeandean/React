10e65dcd1b82ffd5f176046e8e5ef5ab7148c78a
heedeandean <7942ahj@hanmail.net>
2021-02-27 20:41:03 +0900

    2021-02-27

--- client/src/ReactstrapForm.js
+++ client/src/ReactstrapForm.js
@@ -6,7 +6,32 @@ class ReactstrapForm extends Component {
     render() {
         return (
             <Form>
-                <Label for
+                <Label for="exampleGender">gender</Label>
+                <Input type="select" bsSize="sm">
+                    <option>no select</option>
+                    <option>woman</option>
+                    <option>man</option>
+                </Input>
+                <Row Form>
+                    <Col md={6}>
+                        <FormGroup>
+                            <Label for="exampleAddress">address</Label>
+                            <Input type="text" name="address" id="address" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={4}>
+                        <FormGroup>
+                            <Label for="exampleMobile">mobile</Label>
+                            <Input type="text" name="mobile" id="mobile" />
+                        </FormGroup>
+                    </Col>
+                    <Col md={2}>
+                        <FormGroup>
+                            <Label for="exampleAge">age</Label>
+                            <Input type="text" name="age" id="age" />
+                        </FormGroup>
+                    </Col>
+                </Row>
             </Form>
         )
     }
